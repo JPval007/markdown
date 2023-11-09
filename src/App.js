a61556dda2a251f0b7caa39fda5cde5657707e4f
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-09 10:50:20 -0600

    Markdown project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container-sm text-center'>
+      <h1>Hello World!</h1>
+      <i className="fa-brands fa-youtube"></i>
+      <i className="fa-brands fa-twitter"></i>
+      <i className="fa-brands fa-tumblr"></i>
     </div>
+      
+    </>
   );
 }
 
